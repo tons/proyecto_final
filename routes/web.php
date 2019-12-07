07c0a3e9a558c11e81b6854ecc8038c9001398e1
Gaston <gastonfontaine@gmail.com>
2019-12-07 18:19:57 -0300

    Agrega IsAdmin para el logueo al administrador
    eliminé archivos de migrate que se habían duplicado

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,20 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('home');
-});
+});*/
+
+Route::view('/', 'welcome');
 
 Auth::routes();
+
+Route::get('/home', 'HomeController@index')
+    ->name('home');
+
+Route::get('/admin', 'AdminController@admin')
+    ->middleware('is_admin')
+    ->name('admin');
 
 /*
 Route::get('/home', 'HomeController@index')->name('home');

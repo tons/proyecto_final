9d4906d22318a45956bc09db45c3493dd2035c4b
dnandez@gmail.com.ar <dnandez@gmail.com>
2019-12-07 14:52:14 -0300

    migraciones final

---
+++ database/migrations/2019_12_06_230804_create_brands_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateBrandsTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('brands', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name', 255);
+			$table->enum('status', array('Active', 'Deleted'));
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('brands');
+	}
+}
---
+++ database/migrations/2019_12_06_230804_create_orders_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOrdersTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('orders', function(Blueprint $table) {
+			$table->increments('id');
+			$table->integer('user_id')->unsigned()->index();
+			$table->integer('shipping_id')->unsigned()->index();
+			$table->integer('payment_id')->unsigned()->index();
+			$table->integer('total');
+			$table->enum('status', array('Pending', 'Completed'));
+			$table->string('date');
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('orders');
+	}
+}

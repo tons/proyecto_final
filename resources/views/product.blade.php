a61202cf92ad323c5bd48d07c26e6806f888daae
Belen Schmid <belenyschmid@gmail.com>
2019-12-11 18:19:32 -0300

    sacando topheader y poniendo codigo en producto.

---
+++ resources/views/product.blade.php
@@ -0,0 +1,99 @@
+<section id="productoListHome">
+            <div class="container">
+                <div class="row bar">
+                    <div class="col-md-12">
+                        <p class="text-muted lead text-center">Los destacados de <?= date('F') ?></p>
+                        <div class="products-big">
+                            <div class="row products">
+                                <div class="col-lg-3 col-md-4">
+                                    <div class="product">
+                                        <div class="image"><a href="shop-detail.html"><img src="img/product1.jpg" alt="" class="img-fluid image1"></a></div>
+                                        <div class="text">
+                                            <h3 class="h5"><a href="shop-detail.html">Fur coat with very but very very long name</a></h3>
+                                            <p class="price">$143.00</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-4">
+                                    <div class="product">
+                                        <div class="image"><a href="shop-detail.html"><img src="img/product2.jpg" alt="" class="img-fluid image1"></a></div>
+                                        <div class="text">
+                                            <h3 class="h5"><a href="shop-detail.html">White Blouse Armani</a></h3>
+                                            <p class="price">
+                                                <del>$280</del> $143.00
+                                            </p>
+                                        </div>
+                                        <div class="ribbon-holder">
+                                            <div class="ribbon sale">SALE</div>
+                                            <div class="ribbon new">NEW</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-4">
+                                    <div class="product">
+                                        <div class="image"><a href="shop-detail.html"><img src="img/product3.jpg" alt="" class="img-fluid image1"></a></div>
+                                        <div class="text">
+                                            <h3 class="h5"><a href="shop-detail.html">Black Blouse Versace</a></h3>
+                                            <p class="price">$143.00</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-4">
+                                    <div class="product">
+                                        <div class="image"><a href="shop-detail.html"><img src="img/product4.jpg" alt="" class="img-fluid image1"></a></div>
+                                        <div class="text">
+                                            <h3 class="h5"><a href="shop-detail.html">Black Blouse Versace</a></h3>
+                                            <p class="price">$143.00</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-4">
+                                    <div class="product">
+                                        <div class="image"><a href="shop-detail.html"><img src="img/product3.jpg" alt="" class="img-fluid image1"></a></div>
+                                        <div class="text">
+                                            <h3 class="h5"><a href="shop-detail.html">White Blouse Armani</a></h3>
+                                            <p class="price">
+                                                <del>$280</del> $143.00
+                                            </p>
+                                        </div>
+                                        <div class="ribbon-holder">
+                                            <div class="ribbon sale">SALE</div>
+                                            <div class="ribbon new">NEW</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-4">
+                                    <div class="product">
+                                        <div class="image"><a href="shop-detail.html"><img src="img/product4.jpg" alt="" class="img-fluid image1"></a></div>
+                                        <div class="text">
+                                            <h3 class="h5"><a href="shop-detail.html">White Blouse Versace</a></h3>
+                                            <p class="price">$143.00</p>
+                                        </div>
+                                        <div class="ribbon-holder">
+                                            <div class="ribbon new">NEW</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-4">
+                                    <div class="product">
+                                        <div class="image"><a href="shop-detail.html"><img src="img/product2.jpg" alt="" class="img-fluid image1"></a></div>
+                                        <div class="text">
+                                            <h3 class="h5"><a href="shop-detail.html">White Blouse Versace</a></h3>
+                                            <p class="price">$143.00</p>
+                                        </div>
+                                        <div class="ribbon-holder">
+                                            <div class="ribbon new">NEW</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-4">
+                                    <div class="product">
+                                        <div class="image"><a href="shop-detail.html"><img src="img/product1.jpg" alt="" class="img-fluid image1"></a></div>
+                                        <div class="text">
+                                            <h3 class="h5"><a href="shop-detail.html">Fur coat</a></h3>
+                                            <p class="price">$143.00</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>

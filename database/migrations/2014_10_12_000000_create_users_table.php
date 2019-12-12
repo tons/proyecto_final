89c3c70368fd2233cf731379d491232df190498e
Gaston <gastonfontaine@gmail.com>
2019-12-11 22:09:04 -0300

    Login para el admin, avance

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id', 10);
             $table->string('name', 255);
-            $table->string('last_name', 255);
+            $table->string('last_name', 255)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

163a8d793271edc76c7f58e88e629a55b51e92e8
dnandez@gmail.com.ar <dnandez@gmail.com>
2019-12-15 16:33:47 -0300

    crud_category

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::get('/ship', function () {
 
 //Crud marcas
 // Listado
-Route::get('/cms/brands', 'BrandModelController@list')->name('cmsBrands');
+Route::get('/cms/brands', 'BrandModelController@list')->name('cmsMarcas');
 // Mostrar formulario de Creación / Edición
 Route::get('/cms/brands/create_update/{id?}', 'BrandModelController@createOrEdit')->name('cms.brands.createOrUpdate');
 // Obtener datos enviados y Crear/Guardar
@@ -64,18 +64,47 @@ Route::get('/checkout', function () {
 });
 
 
+//Crud categorias
+// Listado
+Route::get('/cms/categories', 'CategoryModelController@list')->name('cmsCategorias');
+// Mostrar formulario de categories/ Edición
+Route::get('/cms/categories/create_update/{id?}', 'CategoryModelController@createOrEdit')->name('cms.categories.createOrUpdate');
+// Obtener datos enviados y Crear/Guardar
+Route::post('/cms/categories/create_update/{id?}', 'CategoryModelController@storeOrUpdate')->name('cms.categories.createOrUpdate');
+// Eliminación
+Route::delete('/cms/categories/delete/{id}', 'CategoryModelController@delete')->name('cms.categories.delete');
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+/*
+//Crud usuarios
+// Listado
+Route::get('/cms/users', 'CustomerModelController@list')->name('cmsUsuarios');
+// Mostrar formulario de Creación / Edición
+Route::get('/cms/users/create_update/{id?}', 'CustomerModelController@createOrEdit')->name('cms.users.createOrUpdate');
+// Obtener datos enviados y Crear/Guardar
+Route::post('/cms/users/create_update/{id?}', 'CustomerModelController@storeOrUpdate')->name('cms.users.createOrUpdate');
+// Eliminación
+Route::delete('/cms/users/delete/{id}', 'CustomerModelController@delete')->name('cms.users.delete');
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+
 
 
 /*
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::resource('customermodel', 'CustomerModelController');
+
 Route::resource('shippingmodel', 'ShippingModelController');
 Route::resource('ordermodel', 'OrderModelController');
 Route::resource('paymentmodel', 'PaymentModelController');
 Route::resource('orderdetailsmodel', 'OrderDetailsModelController');
-Route::resource('categorymodel', 'CategoryModelController');
 */
 
 /*
@@ -83,3 +112,4 @@ Route::get('search/{tipo}/{id}', 'ClasificationController@getClasification')->na
 buscarporclasificacion/category/1
 buscarporclasificacion/tag/1
 */
+?>

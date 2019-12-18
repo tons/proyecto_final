2fbfaedf844c06ace6cc4caca0058a077eec2a77
Santiago <santiago.jofre@osde.com.ar>
2019-12-17 22:03:24 -0300

    Vistas Santi

--- resources/views/layouts/breadcrumbs.blade.php
+++ resources/views/layouts/breadcrumbs.blade.php
@@ -2,12 +2,12 @@
     <div class="container">
         <div class="row d-flex align-items-center flex-wrap">
             <div class="col-md-7">
-                <h1 class="h2">Checkout - Delivery Method</h1>
+                <h1 class="h2">Checkout {{--Delivery Method--}}</h1>
             </div>
             <div class="col-md-5">
                 <ul class="breadcrumb d-flex justify-content-end">
                     <li class="breadcrumb-item"><a href="-back-template/index-back.html">Home</a></li>
-                    <li class="breadcrumb-item active">Checkout - Delivery Method</li>
+                    <li class="breadcrumb-item active">Checkout {{-- Delivery Method--}}</li>
                 </ul>
             </div>
         </div>
